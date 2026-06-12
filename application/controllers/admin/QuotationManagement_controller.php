dcf551e81ca6f987563c3aef09478ab04b55944d
Khang Nguyen <nguyennhukhangvn@gmail.com>
2026-06-12 15:36:47 +0700

    integrate QR Code

--- application/controllers/admin/QuotationManagement_controller.php
+++ application/controllers/admin/QuotationManagement_controller.php
@@ -19,6 +19,7 @@ class QuotationManagement_controller extends MY_Controller
 		$this->load->helper("bootstrap_pagination_admin");
 		$this->load->helper("seo_url");
 		$this->load->library('form_validation');
+		$this->load->helper("my_email");
 	}
 
 	public function index()
@@ -28,6 +29,24 @@ class QuotationManagement_controller extends MY_Controller
 		if(!$config['orderField']){
 			$config['orderField'] = "RequestedDate";
 			$config['orderDirection'] = "DESC";
+		}
+		$crucation = $this->input->post("crudaction");
+		if($crucation != null && $crucation == 'send-mail'){
+			$quoteId = $this->input->post("quoteId");
+			$quotation = $this->Quotation_Model->findById($quoteId);
+			$quote = $quotation['quote'];
+			$customerEmail = $quote->Email;
+			$code = $quote->Code;
+
+			if($customerEmail != null && strlen($customerEmail) > 0){
+				$html_message = $this->load->view('admin/templates/quotation_view', $quotation, TRUE);
+				my_send_email($customerEmail, "Vân Anh Shop - Báo Giá - ".$code, $html_message );
+				$data['message_response'] = 'Gửi báo giá thành công.';
+			}
+		} else if($crucation != null && $crucation == 'delete'){
+			$quoteId = $this->input->post("quoteId");
+			$this->Quotation_Model->deleteById($quoteId);
+			$data['message_response'] = 'Xóa báo giá thành công.';
 		}
 
 		$status = $this->input->get('status');
@@ -50,8 +69,11 @@ class QuotationManagement_controller extends MY_Controller
 			$shippingFee = $this->input->post("ShippingFee");
 			$discount = $this->input->post("Discount");
 			$validDate = $this->input->post("valid_date");
-			$this->Quotation_Model->updateQuote($quoteId, $loginId, $shippingFee, $discount, $validDate, $quotes);
-			$msg = "Đã cập nhật báo giá thành công";
+			$this->form_validation->set_rules("valid_date", "Ngày hiệu lực", "required");
+			if ($this->form_validation->run()){
+				$this->Quotation_Model->updateQuote($quoteId, $loginId, $shippingFee, $discount, $validDate, $quotes);
+				$msg = "Đã cập nhật báo giá thành công";
+			}
 		} else if($crucation != null && $crucation == 'approved'){
 			$this->Quotation_Model->updateStatus(QUOTE_STATUS_APPROVED, $loginId, $quoteId);
 			$msg = "Đã cập nhật báo giá thành công";
