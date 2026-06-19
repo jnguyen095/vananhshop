3f1b3287b81443fab580495a9e9779d604cc17c5
Khang Nguyen <nguyennhukhangvn@gmail.com>
2026-06-19 21:23:08 +0700

    Add search on header

--- application/views/search/Search_view.php
+++ application/views/search/Search_view.php
@@ -1,16 +1,9 @@
 <!DOCTYPE html>
 <html lang = "en">
-<?php
-	$searchBy = "";
-	if(isset($category) && !isset($cat_city) && !isset($cat_city_dic)){
-		$searchBy = $category->CatName;
-	} else{
-		$searchBy = "Tìm kiếm sản phẩm | Vân Anh Shop";
-	}
-	?>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title><?=$searchBy?></title>
+	<title>Tìm kiếm sản phẩm<?=(isset($query) && strlen($query)) ? ' - '. $query.(isset($category) ? ', ' . $category->CatName : "") : (isset($category) ? ', ' . $category->CatName : "")?> | Vân Anh Shop</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php $this->load->view('common_header')?>
 	<?php $this->load->view('/common/googleadsense')?>
 	<?php $this->load->view('/common/facebook-pixel-tracking')?>
@@ -21,40 +14,42 @@
 <div class="container-fluid no-padding-left no-padding-right">
 
 <?php $this->load->view('/theme/header')?>
-
-
 	<ul itemscope itemtype="http://schema.org/BreadcrumbList" class="breadcrumb">
 		<div class="container">
 			<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="<?=base_url().'trang-chu.html'?>"><span itemprop="name">Trang Chủ</span></a><meta itemprop="position" content="1" /></li>
 			<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" class="active"><span itemprop="item"><span itemprop="name">Tìm kiếm</span></span><meta itemprop="position" content="2" /></li>
 		</div>
 	</ul>
-
+	<?php
+	$attributes = array("name" => "search", "id" => "search_form", "class" => "custom-input");
+	echo form_open("tim-kiem", $attributes);
+	?>
 	<div class="container">
 		<div class="row no-margin">
-			<div class="search-result col-md-9 no-margin no-padding">
-
-			</div>
-			<div class="col-md-9 no-margin no-padding">
-				<div class="search-result-panel col-md-12">
-					<?=number_format($total)?> kết quả<span class="search-total-result">
+			<div class="search-result-panel col-md-12">
+				<?=number_format($total)?> kết quả<span class="search-total-result">
 					<?php
-					 $str = '';
-					 if(isset($keyword) && strlen($keyword) > 0){
-						 $str .= ' "'.$keyword.'"';
-					 }
-					 if(isset($category)){
-						 if(isset($keyword)){
-							 $str .= ', '.$category->CatName;
-						 }else {
-							 $str .= $category->CatName;
-						 }
-					 }
+					$str = '';
+					if(isset($query) && strlen($query) > 0){
+						$str .= '\''.$query.'\'';
+					}
+					if(isset($category)){
+						if(isset($query)){
+							$str .= ', '.$category->CatName;
+						}else {
+							$str .= $category->CatName;
+						}
+					}
 
-					 echo $str;
+					echo $str;
 					?>
 					</span>
-				</div>
+			</div>
+		</div>
+
+		<div class="row no-margin">
+			<div class="col-md-9 no-margin no-padding">
+
 				<div class="product-panel col-md-12 no-margin no-padding">
 					<div class="row">
 						<?php
@@ -94,15 +89,52 @@
 				</div>
 			</div>
 			<div class="col-md-3 no-margin-right no-padding-right no-padding-left-mobile">
-				<?php $this->load->view('/common/user_author') ?>
-				<?php $this->load->view('/common/branch-left') ?>
-				<?php $this->load->view('/common/district-left-link')?>
-				<?php $this->load->view('/common/branch-left-link')?>
-				<?php $this->load->view('/common/Search_filter') ?>
+				<div class="search-panel block-panel">
+					<div class="block-header">TÌM KIẾM SẢN PHẨM</div>
+
+					<div class="block-body">
+						<div class="row">
+							<input id="keyword" type="text" placeholder="Từ khóa" value="<?=isset($query) ? $query : ''?>" name="query"/>
+						</div>
+						<div class="row">
+							<select id="cmCatId" name="cmCatId">
+								<option value="-1">Tất cả danh mục</option>
+								<?php
+								if($categories != null && count($categories) > 0){
+									foreach ($categories as $c){
+										?>
+										<option value="<?=$c['CategoryID']?>" <?=((isset($categoryId) && $categoryId == $c['CategoryID']) ? ' selected="selected"' : '')?> ><?=$c['CatName']?></option>
+										<?php
+										if(count($c['nodes']) > 0){
+											foreach ($c['nodes'] as $k){
+												?>
+												<option value="<?=$k['CategoryID']?>" <?=((isset($categoryId) && $categoryId == $k['CategoryID']) ? ' selected="selected"' : '')?> >&nbsp;&nbsp;&nbsp;&nbsp;<?=$k['CatName']?></option>
+												<?php
+											}
+										}
+									}
+								}
+								?>
+							</select>
+						</div>
+
+						<div class="row text-center">
+							<a id="btnDetailSearch" class="btn btn-tindatdai btn-sm"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Tìm Kiếm</a>
+						</div>
+					</div>
+
+				</div>
 			</div>
 		</div>
-
 	</div>
+	<?php echo form_close(); ?>
+	<script type="text/javascript">
+		$("#btnDetailSearch").click(function () {
+			var searchKey = $('#keyword').val()||"";
+			var catId = $('#cmCatId').val()||"";
+			window.location.href = '<?=base_url('/tim-kiem.html')?>?query='+searchKey + '&category=' + catId;
+		});
+	</script>
 </div>
 <?php $this->load->view('/theme/footer')?>
 </body>
